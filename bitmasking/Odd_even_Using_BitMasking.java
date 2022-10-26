53b8989c253531157a9e4aea8627d220d1f66e8d
Sakshi Mishra <84084388+sam2611@users.noreply.github.com>
2022-10-26 18:13:26 +0530

    Update Odd_even_Using_BitMasking.java
    did update 89 to 99

--- bitmasking/Odd_even_Using_BitMasking.java
+++ bitmasking/Odd_even_Using_BitMasking.java
@@ -5,7 +5,7 @@ public class Odd_even_Using_BitMasking {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int n = 89;
+		int n = 99;
 		if ((n & 1) == 0) {
 			System.out.println("Even");
 		} else {
